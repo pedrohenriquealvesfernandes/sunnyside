53de4cfd4b22e24cc2fc5ebe45176eb1579c38d2
Pedro Henrique Alves <118781462+pedrohenriquealvesfernandes@users.noreply.github.com>
2023-03-22 21:07:48 -0300

    projeto finalizado

---
+++ menu.js
@@ -0,0 +1,9 @@
+function clicou(){
+    if (lista.style.display=='flex'){
+        lista.style.display = 'none'
+        headert.style.display = 'block'    
+    }else{
+       lista.style.display = 'flex' 
+       headert.style.display = 'none'
+    }
+}
